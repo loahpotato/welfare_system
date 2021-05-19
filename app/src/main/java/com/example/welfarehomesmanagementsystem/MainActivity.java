51c6f2f24ff5d30cce89b8a028a99d107508e324
刘立恒 <1241918065@qq.com>
2021-05-19 11:24:48 +0800

    初步搭建Home,Functions,Account,Settings四页。 完成通过底部导航栏实现页面跳转功能

--- app/src/main/java/com/example/welfarehomesmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.welfarehomesmanagementsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavi = findViewById(R.id.navigation_bottom);
+        bottomNavi.setOnNavigationItemSelectedListener(naviListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener naviListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.navi_home:
+                    fragment = new HomeActivity();
+                    break;
+                case R.id.navi_functions:
+                    fragment = new FunctionsActivity();
+                    break;
+                case R.id.navi_account:
+                    fragment = new AccountActivity();
+                    break;
+                case R.id.navi_settings:
+                    fragment = new SettingActivity();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return false;
+        }
+    };
 }
