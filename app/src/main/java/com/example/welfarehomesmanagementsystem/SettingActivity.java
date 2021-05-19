6005d37b9755501545ceb46ab6c19b3bff3e7fa8
pattonye <SWE1809382@xmu.edu.my>
2021-05-19 22:33:33 +0800

    ypd

--- app/src/main/java/com/example/welfarehomesmanagementsystem/SettingActivity.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/SettingActivity.java
@@ -16,4 +16,5 @@ public class SettingActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_setting, container, false);
     }
+    //测试修改
 }
