a94b118f7b2e52836ca47277d949c1a9cce7a8c6
刘立恒 <1241918065@qq.com>
2021-05-28 14:48:12 +0800

    完善aboutus页面，增加拨打电话功能

--- app/src/main/java/com/example/welfarehomesmanagementsystem/AboutUsActivity.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/AboutUsActivity.java
@@ -2,7 +2,11 @@ package com.example.welfarehomesmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        TextView contactNum = (TextView) findViewById(R.id.contactnum);
+        contactNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:" + contactNum.getText().toString()));
+                startActivity(i);
+            }
+        });
     }
 }
