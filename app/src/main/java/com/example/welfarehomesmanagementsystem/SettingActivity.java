56eb63eedd98785e00a9b32e3e11c5dbc984b4d5
刘立恒 <1241918065@qq.com>
2021-05-31 11:08:36 +0800

    初步编写Policy页面

--- app/src/main/java/com/example/welfarehomesmanagementsystem/SettingActivity.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/SettingActivity.java
@@ -13,17 +13,26 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class SettingActivity extends Fragment {
-    private TextView about;
+    private TextView about,policy;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_setting, container, false);
         //code here
         about = view.findViewById(R.id.aboutus);
+        policy = view.findViewById(R.id.policy);
         about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getContext(),AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),PolicyActivity.class);
                 startActivity(i);
             }
         });
