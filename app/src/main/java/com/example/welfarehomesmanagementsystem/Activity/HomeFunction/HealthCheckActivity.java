11d96b7d906c23aa02d6dd5806292ade036c6a93
刘立恒 <1241918065@qq.com>
2021-06-22 17:04:50 +0800

    为体检预约新建表，新增DbHelper_HealthCheck

--- app/src/main/java/com/example/welfarehomesmanagementsystem/Activity/HomeFunction/HealthCheckActivity.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/Activity/HomeFunction/HealthCheckActivity.java
@@ -4,24 +4,64 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.welfarehomesmanagementsystem.Activity.MainActivity;
+import com.example.welfarehomesmanagementsystem.Activity.SignUpActivity;
+import com.example.welfarehomesmanagementsystem.DatabaseHelper;
+import com.example.welfarehomesmanagementsystem.DbHelper_HealthCheck;
 import com.example.welfarehomesmanagementsystem.R;
 import com.example.welfarehomesmanagementsystem.widget.TitleLayout;
 
 import java.util.Calendar;
 
 public class HealthCheckActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
-    EditText date;
+    EditText date,name,age,contact,hospital;
+    Button submit;
     TextView chooseDate;
+    private DbHelper_HealthCheck DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_check);
         TitleLayout t=findViewById(R.id.title_health_check);
         t.setT(R.string.healthcheck);
+        name = findViewById(R.id.patients_name);
+        date = findViewById(R.id.appoint_date);
+        age = findViewById(R.id.patients_age);
+        contact = findViewById(R.id.contact_number);
+        hospital = findViewById(R.id.hospital_addr);
+        submit = findViewById(R.id.healthcheck_submit);
+        DB = new DbHelper_HealthCheck(this);
+        AddData();
+
+    };
+    public void AddData(){
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name1,age1,contact1,hospital1,date1;
+                name1=name.getText().toString(); age1=age.getText().toString();contact1=contact.getText().toString();hospital1=hospital.getText().toString();date1=date.getText().toString();
+
+                if (name1.equals("")||age1.equals("")||contact1.equals("")||hospital1.equals("")||date1.equals("")) {
+                    Toast.makeText(HealthCheckActivity.this, "Please enter all fields", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    boolean isInserted = DB.insertData(name.getText().toString(),date.getText().toString(),age.getText().toString(),contact.getText().toString(),hospital.getText().toString());
+                    if(isInserted == true)
+                        Toast.makeText(HealthCheckActivity.this,"Make appoint successfully!",Toast.LENGTH_SHORT).show();
+                        else
+                        Toast.makeText(HealthCheckActivity.this,"Fail!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -30,8 +70,6 @@ public class HealthCheckActivity extends AppCompatActivity implements DatePicker
         chooseDate = findViewById(R.id.tv_date);
         date = findViewById(R.id.appoint_date);
         date.setText(desc);
-
-
         if(chooseDate.getId()==R.id.tv_date){
             //获取实例，包含当前年月日
             Calendar calendar = Calendar.getInstance();
@@ -43,4 +81,4 @@ public class HealthCheckActivity extends AppCompatActivity implements DatePicker
         }
 
     }
-}
+        }
---
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/DbHelper_HealthCheck.java
@@ -0,0 +1,60 @@
+package com.example.welfarehomesmanagementsystem;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper_HealthCheck extends SQLiteOpenHelper {
+    //Declare variable name for database, table and column
+    public static final String DATABASE_NAME = "HealthCheckAppointment.db";
+    public static final String TABLE_NAME = "HealthCheckAppointment_table";
+    public static final String COLUMN_1 = "NAME";
+    public static final String COLUMN_2 = "DATE";
+    public static final String COLUMN_3 = "AGE";
+    public static final String COLUMN_4 = "CONTACT";
+    public static final String COLUMN_5 = "HOSPITAL";
+    public DbHelper_HealthCheck(@Nullable Context context) {
+        super(context,DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+TABLE_NAME+"(NAME TEXT, DATE TEXT, AGE TEXT,CONTACT TEXT,HOSPITAL TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop table if it's already exist
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME );
+        //if not exist, we can parse the onCreate method to run
+        onCreate(db);
+    }
+    public boolean insertData(String name,String date, String age,String contact, String hospital){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_1,name);
+        contentValues.put(COLUMN_2,date);
+        contentValues.put(COLUMN_3,age);
+        contentValues.put(COLUMN_4,contact);
+        contentValues.put(COLUMN_5,hospital);
+
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result ==-1)
+            return false;
+        else
+            return true;
+    }
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("SELECT * FROM "  + TABLE_NAME, null);
+        return result;
+    }
+    public Integer deleteData(String name){
+        SQLiteDatabase db =getWritableDatabase();
+        return db.delete(TABLE_NAME,"NAME = ?", new String[]{name});
+    }
+}
