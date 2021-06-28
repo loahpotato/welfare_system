5cb323c29c4286a897f17160342739b181ea0e25
Loahpotato <964040805@qq.com>
2021-06-28 16:38:33 +0800

    完成profile功能

--- app/src/main/java/com/example/welfarehomesmanagementsystem/Activity/ProfileActivity.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/Activity/ProfileActivity.java
@@ -1,19 +1,99 @@
 package com.example.welfarehomesmanagementsystem.Activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.welfarehomesmanagementsystem.Activity.HomeFunction.HealthResultActivity;
+import com.example.welfarehomesmanagementsystem.DatabaseHelper;
 import com.example.welfarehomesmanagementsystem.R;
+import com.example.welfarehomesmanagementsystem.widget.ItemGroup;
 import com.example.welfarehomesmanagementsystem.widget.TitleLayout;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private TextView title_name;
+    private ItemGroup id,name,position,gender,age,phone;
+    private SharedPreferences pref ;
+    private DatabaseHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         TitleLayout t=findViewById(R.id.title_profile);
         t.setT(R.string.profile);
+
+        title_name=findViewById(R.id.profile_name);
+        id=findViewById(R.id.ig_id);
+        name=findViewById(R.id.ig_name);
+        position=findViewById(R.id.ig_position);
+        gender=findViewById(R.id.ig_gender);
+        age=findViewById(R.id.ig_age);
+        phone=findViewById(R.id.ig_phone);
+
+        DB = new DatabaseHelper(this);
+        pref= getSharedPreferences("CurrentUserId",MODE_PRIVATE);
+        String uid = pref.getString("currentUserId","");
+
+        Cursor result = DB.getUserById(uid);
+        while(result.moveToNext()) {
+            title_name.setText(result.getString(1));
+            id.setContent(uid);
+            name.setContent(result.getString(1));
+            if(result.getInt(3)==0)
+                position.setContent("Staff");
+            else
+                position.setContent("Manager");
+            gender.setContent(result.getString(6));
+            age.setContent(result.getString(4));
+            phone.setContent(result.getString(5));
+        }
+        age.getJtRightIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, UpdateProfileActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+        phone.getJtRightIv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, UpdateProfileActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String re_age = data.getStringExtra("age");
+                    String re_phone = data.getStringExtra("phone");
+                    if(!re_age.equals(""))
+                        age.setContent(re_age);
+                    if(!re_phone.equals(""))
+                        phone.setContent(re_phone);
+                }
+                break;
+            default:
+        }
     }
 }
--- app/src/main/java/com/example/welfarehomesmanagementsystem/widget/ItemGroup.java
+++ app/src/main/java/com/example/welfarehomesmanagementsystem/widget/ItemGroup.java
@@ -1,12 +1,20 @@
 package com.example.welfarehomesmanagementsystem.widget;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.method.KeyListener;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -23,10 +31,14 @@ public class ItemGroup extends FrameLayout{
     private LinearLayout itemGroupLayout; //组合控件的布局
     private TextView titleTv; //标题
     private TextView contentEdt; //输入框
-    private ImageView jtRightIv; //向右的箭头
+    private ImageButton jtRightIv; //向右的箭头
 
-    public TextView getContentEdt() {
-        return contentEdt;
+    public ImageView getJtRightIv() {
+        return jtRightIv;
+    }
+
+    public void setContent(String contentEdt) {
+        this.contentEdt.setText(contentEdt);
     }
 
     public ItemGroup(@NonNull Context context) {
@@ -52,10 +64,11 @@ public class ItemGroup extends FrameLayout{
         itemGroupLayout = (LinearLayout) view.findViewById(R.id.item_group_layout);
         titleTv = (TextView) view.findViewById(R.id.title_tv);
         contentEdt = (TextView) view.findViewById(R.id.content_edt);
-        jtRightIv = (ImageView) view.findViewById(R.id.jt_right_iv);
+        jtRightIv = (ImageButton) view.findViewById(R.id.jt_right_iv);
         addView(view); //把自定义的这个组合控件的布局加入到当前FramLayout
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void initAttrs(Context context, AttributeSet attrs) {
         int defaultTitleColor = context.getResources().getColor(R.color.gray);
         int defaultEdtColor = context.getResources().getColor(R.color.black);
@@ -67,10 +80,10 @@ public class ItemGroup extends FrameLayout{
         float paddingRight = typedArray.getDimension(R.styleable.ItemGroup_paddingRight, 15);
         float paddingTop = typedArray.getDimension(R.styleable.ItemGroup_paddingTop, 5);
         float paddingBottom = typedArray.getDimension(R.styleable.ItemGroup_paddingTop, 5);
-        float titleSize = typedArray.getDimension(R.styleable.ItemGroup_title_size, 15);
+        float titleSize = typedArray.getDimension(R.styleable.ItemGroup_title_size, 17);
         int titleColor = typedArray.getColor(R.styleable.ItemGroup_title_color, defaultTitleColor);
         String content = typedArray.getString(R.styleable.ItemGroup_edt_content);
-        float contentSize = typedArray.getDimension(R.styleable.ItemGroup_edt_text_size, 13);
+        float contentSize = typedArray.getDimension(R.styleable.ItemGroup_edt_text_size, 14);
         int contentColor = typedArray.getColor(R.styleable.ItemGroup_edt_text_color, defaultEdtColor);
         String hintContent = typedArray.getString(R.styleable.ItemGroup_edt_hint_content);
         int hintColor = typedArray.getColor(R.styleable.ItemGroup_edt_hint_text_color, defaultHintColor);
@@ -92,9 +105,10 @@ public class ItemGroup extends FrameLayout{
         contentEdt.setTextColor(contentColor);
         contentEdt.setHint(hintContent);
         contentEdt.setHintTextColor(hintColor);
-//        contentEdt.setFocusable(isEditable); //设置输入框是否可以编辑
-//        contentEdt.setClickable(true);
-//        contentEdt.setKeyListener(null);
+
+        //contentEdt.setKeyListener();
         jtRightIv.setVisibility(showJtIcon ? View.VISIBLE : View.GONE);  //设置向右的箭头图标是否可见
     }
+
+
 }
